15dbe72db4b096b1d23775e5776b017479ee4566
shermanng10 <sherman.ng10@gmail.com>
2016-08-08 23:31:23 -0400

    Add listener to handle generation of vote event and randomly selecting a lunch leader

--- src/helpers/BotHelpers.js
+++ src/helpers/BotHelpers.js
@@ -1,6 +1,8 @@
 import MessageParser from '../helpers/MessageParser'
 import botController from '../lib/Bot'
 import Yelp from '../lib/Yelp'
+import VoteEvent from '../models/VoteEvent'
+import Channel from '../models/Channel'
 
 const yelp = new Yelp()
 
@@ -8,17 +10,76 @@ function searchHandler(bot, message){
 	try {
 		let searchParams = MessageParser.getSearchTerms(message)
 		yelp.getRestaurants(searchParams).then((dataArray) => { 
-			for (let restaurant of dataArray) {
-				bot.reply(message, restaurant.name)
-			}
+			botController.storage.channels.get(message.channel, (err, data) => {
+				console.log(data)
+				let channel = Channel.toObj(data)
+				let voteEvent = channel.getVoteEvent()
+				if (message.user != voteEvent.getLunchLeader()){
+					for (let restaurant of dataArray){
+						bot.reply(message, restaurant.name)
+					}
+				} else {
+					bot.reply(message, 'you da leader')
+				}
+			})
 		})
 	} catch (e){
 		bot.reply(message, e.message)
 	}
 }
 
-function randomizeTeamLeader(bot, message){
-
+function startVoteEvent(bot, message){
+	botController.storage.channels.get(message.channel, (err, data) => { 
+		let channel = Channel.toObj(data).startVoteEvent()
+		botController.storage.channels.save(channel)
+	   	bot.say({
+	   			text: '',
+	   			channel: message.channel,
+	        	attachments:[
+		            {
+		                title: 'A lunch event has been started! Please confirm within 5 minutes if you would like to be part of the vote.',
+		                callback_id: 'joinEvent',
+		                attachment_type: 'default',
+		                actions: [
+		                    {
+		                        "name":"yes",
+		                        "text": "Yes",
+		                        "value": "yes",
+		                        "type": "button",
+		                    },
+		                    {
+		                        "name":"no",
+		                        "text": "No",
+		                        "value": "no",
+		                        "type": "button",
+		                    }
+		                ]
+		            }
+	        ]
+	    }, (err, botReply) => {
+	    	setTimeout(() => {
+	    		bot.api.chat.update({as_user: true, ts: botReply.ts, channel: botReply.channel, text: 'Voting has closed!', attachments: '[]'})
+	 			botController.storage.channels.get(message.channel, (err, data) => {
+	 				let message_text = 'Not enough users joined the vote! At least one person needs to join.'
+	 				let channel = Channel.toObj(data)
+	 				let voteEvent = channel.getVoteEvent()
+					let lunchLeader = voteEvent.randomizeLeader()
+					botController.storage.channels.save(channel)
+					bot.api.users.info({user: lunchLeader}, (err, data) => {
+						if (err) {
+							console.log(err)
+						} else {
+							let userName = data.user.name
+							message_text = `${userName} has been randomly selected as the lunch leader!`
+						}
+						bot.say({text: message_text,
+							 channel: botReply.channel,
+						})
+					})
+	 			})
+	    	}, 7000)
+	    })
+	})
 }
 
-export {searchHandler}
+export {searchHandler, startVoteEvent}
--- src/lib/Bot.js
+++ src/lib/Bot.js
@@ -1,18 +1,19 @@
 import Botkit from 'botkit'
 import Yelp from './Yelp'
-import {searchHandler} from '../helpers/BotHelpers'
+import {searchHandler, startVoteEvent} from '../helpers/BotHelpers'
 import MongoDB from 'botkit-storage-mongo'
 import Channel from '../models/Channel'
 
 const mongoStorage = new MongoDB({mongoUri: 'mongodb://localhost:27017/foodbot'})
 
 const botController = Botkit.slackbot({
-	storage: mongoStorage
+	storage: mongoStorage,
+	interactive_replies: true
 }).configureSlackApp(
   {
   	clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    scopes: ['bot'],
+    scopes: ['bot', 'users:read'],
   }
 )
 
@@ -30,11 +31,31 @@ botController.setupWebserver(process.env.PORT, (err,webserver) => {
 botController.on('channel_joined', (bot, message) => {
 	bot.api.channels.info({channel: message.channel.id}, (err, res) => {
 		let channel = res.channel
-		let cChannel = new Channel({id: channel.id, name: channel.name, members: channel.members})
+		let cChannel = new Channel({id: channel.id, _name: channel.name, _members: channel.members})
 		botController.storage.channels.save(cChannel)
 	})
 })
 
+botController.on('interactive_message_callback', (bot, message) => {
+	if (message.callback_id == 'joinEvent'){
+		botController.storage.channels.get(message.channel, (err, data) => {
+			let payload = JSON.parse(message.payload)
+			let channel = Channel.toObj(data)
+			let voteEvent = channel.getVoteEvent()
+			if (message.actions[0].value == 'yes'){
+				voteEvent.addVoter(payload.user.id)
+			} else if (message.actions[0]. value == 'no')
+			{
+				voteEvent.removeVoter(payload.user.id)
+			}
+			botController.storage.channels.save(channel)
+		})
+	}
+})
+
+
+
+botController.hears('start vote', ['direct_message', 'direct_mention'], startVoteEvent)
 
 botController.hears('search', ['direct_message', 'direct_mention'], searchHandler)
 
